f743157c8f493f3e66f2cf894652714dd23c3a22
Lorenzo Marques <lorenzo.marques246@gmail.com>
2022-02-14 11:51:04 -0300

    fix:bordas a tamanhos dos cards concertados, fonte concertada e margin concertada

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,53 +5,58 @@ const Cart = ({ currentSale, remove }) => {
   if (currentSale[0] !== undefined) {
     return (
       <div>
-        <div className="cart-container">
-          <div className="carrinho-de-compras">
-            <h3>Carrinho de Compras</h3>
+        <div className="cart-flex">
+          <div className="cart-container">
+            <div className="carrinho-de-compras">
+              <h3>Carrinho de Compras</h3>
+            </div>
+            {currentSale.map((element) => {
+              return (
+                <Product
+                  product={element}
+                  Children={"Remover"}
+                  HandleClick={remove}
+                  key={element.id}
+                  classname="cart-product"
+                />
+              );
+            })}
+            <div className="total">
+              <h3>Total</h3>
+              <p>
+                R$:{" "}
+                {currentSale
+                  .reduce((acc, cur) => acc + cur.price, 0)
+                  .toString()
+                  .split("")
+                  .slice(0, 5)
+                  .join("")}
+              </p>
+            </div>
+
+            <button
+              className="button-romove-all"
+              onClick={() => {
+                remove(0);
+              }}
+            >
+              Remover Tudo
+            </button>
           </div>
-          {currentSale.map((element) => {
-            return (
-              <Product
-                product={element}
-                Children={"Remover"}
-                HandleClick={remove}
-                key={element.id}
-                classname="cart-product"
-              />
-            );
-          })}
-          <div className="total">
-            <h3>Total</h3>
-            <p>
-              R$:{" "}
-              {currentSale
-                .reduce((acc, cur) => acc + cur.price, 0)
-                .toString()
-                .split("")
-                .slice(0, 5)
-                .join("")}
-            </p>
-          </div>
-          <button
-            className="button-romove-all"
-            onClick={() => {
-              remove(0);
-            }}
-          >
-            Remover Tudo
-          </button>
         </div>
       </div>
     );
   } else {
     return (
       <div>
-        <div className="cart-container-empty">
-          <div className="carrinho-de-compras">
-            <h3>Carrinho de Compras</h3>
+        <div className="cart-flex-empy">
+          <div className="cart-container-empty">
+            <div className="carrinho-de-compras">
+              <h3>Carrinho de Compras</h3>
+            </div>
+            <h3 className="empty-cart-h3">Sua sacola está vazia</h3>
+            <span className="empty-cart-span">Adicione itens</span>
           </div>
-          <h3 className="empty-cart-h3">Sua sacola está vazia</h3>
-          <span className="empty-cart-span">Adicione itens</span>
         </div>
       </div>
     );
